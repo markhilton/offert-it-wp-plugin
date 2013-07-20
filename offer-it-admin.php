ff3bee35a4d37aa42fc77696c170fcfc2f24c295
Mark Hilton <mark@esecure.cc>
2013-07-20 22:50:03 +0000

    Implemented landing page detection in order to process initial hit tracking Modified & upgraded admin interface to feature full required configuration variables

--- offer-it-admin.php
+++ offer-it-admin.php
@@ -4,7 +4,7 @@
 	
 	<form method="post" action="options.php">
 		<input type="hidden" name="action" value="update" />
-		<input type="hidden" name="page_options" value="offer_it_domain,offer_it_username,offer_it_apikey,offer_it_conversion_url,offer_it_conversion_ouid,offer_it_ga_code,offer_it_ga_domain" />
+		<input type="hidden" name="page_options" value="offer_it_domain,offer_it_username,offer_it_apikey,offer_it_tracking_code,offer_it_landing_url,offer_it_conversion_url,offer_it_conversion_ouid,offer_it_db_table,offer_it_db_column,offer_it_log,offer_it_map_total,offer_it_map_first_name,offer_it_map_last_name,offer_it_map_address1,offer_it_map_address2,offer_it_map_city,offer_it_map_state,offer_it_map_zip,offer_it_map_country,offer_it_map_email,offer_it_map_phone,offer_it_map_custom1,offer_it_map_custom2,offer_it_map_custom3,offer_it_map_custom4,offer_it_map_custom5" />
 		
 		<?php wp_nonce_field('update-options'); ?>
 	
@@ -18,7 +18,10 @@
 			</tr>
 			<tr>
 				<th align="left">Tracking domain:</th>
-				<td align="left"><input name="offer_it_domain"   type="text" size="32" value="<?php echo get_option('offer_it_domain'); ?>" /></td>
+				<td align="left">
+					<input name="offer_it_domain" type="text" size="32" value="<?php echo get_option('offer_it_domain'); ?>" />
+					Don't include http://
+				</td>
 			</tr>
 			<tr>
 				<th align="left">User name:</th>
@@ -42,42 +45,193 @@
 				</td>
 			</tr>
 			<tr>
+				<th align="left">Default tracking code:</th>
+				<td align="left">
+					<input name="offer_it_tracking_code"  type="text" size="32" value="<?php echo get_option('offer_it_tracking_code'); ?>" />
+					Default <a target="_blank" href="http://wiki.offerit.com/index.php/Offeritcode">ocode</a> used when URL request does not contain one
+				</td>
+			</tr>
+			<tr>
+				<th align="left">Landing URL:</th>
+				<td align="left">
+					<input name="offer_it_landing_url"  type="text" size="32" value="<?php echo get_option('offer_it_landing_url'); ?>" />
+					(ex. /store/checkout/). URL to trigger tracking redirection.
+				</td>
+			</tr>
+			<tr>
 				<th align="left">Conversion URL:</th>
 				<td align="left">
 					<input name="offer_it_conversion_url"  type="text" size="32" value="<?php echo get_option('offer_it_conversion_url'); ?>" />
-					(ex. /store/receipt/?ouid=)
+					(ex. /store/receipt/). URL to trigger conversion post
 				</td>
 			</tr>
 			<tr>
 				<th align="left">Conversion variable:</th>
 				<td align="left">
 					<input name="offer_it_conversion_ouid" type="text" size="32" value="<?php echo get_option('offer_it_conversion_ouid'); ?>" />
-					(ex. ouid)
+					Unique order ID (ex. ouid)
 				</td>
 			</tr>
 
 
 			<tr>
 				<td colspan="2" align="left">
-					<h3>Google Analytics <i>(optional)</i></h3>
-					<p>This configuration will push custom events in order to track traffic and e-commerce conversions for individual affiliates in your 
-					<a target="_blank" href="www.google.com/analytics/">Google Analytics</a> account.</p>
+					<h3>Database source</h3>
 				</td>
 			</tr>
 			<tr>
-				<th align="left">Property ID:</th>
-				<td align="left"><input name="offer_it_ga_code" type="text" size="32" value="<?php echo get_option('offer_it_ga_code'); ?>" />
-					(ex. UA-12345678-1)
+				<th align="left">Database table:</th>
+				<td align="left">
+					<input name="offer_it_db_table"  type="text" size="32" value="<?php echo get_option('offer_it_db_table'); ?>" />
+					(ex. cart66_orders). Database table name containing processed order information
 				</td>
 			</tr>
 			<tr>
-				<th align="left">Tracking domain:</th>
-				<td align="left"><input name="offer_it_ga_domain" type="text" size="32" value="<?php echo get_option('offer_it_ga_domain'); ?>" />
-					(ex. <?php echo 'mydomain.com' ?>)
+				<th align="left">Database column:</th>
+				<td align="left">
+					<input name="offer_it_db_column"  type="text" size="32" value="<?php echo get_option('offer_it_db_column'); ?>" />
+					(ex. ouid). Database column name containing unique order IDs
 				</td>
+			</tr>
+
+
+			<tr>
+				<td colspan="2" align="left">
+					<h3>Database mapping</h3>
+					<p>This information will be queried from database table configured above in order to construct conversion post to OfferIT system.<br />
+					The example fields are provided based on Cart66 plugin.</p>
+				</td>
+			</tr>
+			<tr>
+				<th align="left">Transaction total:</th>
+				<td align="left">
+					<input name="offer_it_map_total"       type="text" size="32" value="<?php echo get_option('offer_it_map_total'); ?>" />
+					(ex. total). Transaction total
+				</td>
+			</tr>
+			<tr>
+				<th align="left">First Name:</th>
+				<td align="left">
+					<input name="offer_it_map_first_name"  type="text" size="32" value="<?php echo get_option('offer_it_map_first_name'); ?>" />
+					(ex. bill_first_name). Client first name
+				</td>
+			</tr>
+			<tr>
+				<th align="left">Last Name:</th>
+				<td align="left">
+					<input name="offer_it_map_last_name"   type="text" size="32" value="<?php echo get_option('offer_it_map_last_name'); ?>" />
+					(ex. bill_last_name). Client last name
+				</td>
+			</tr>
+			<tr>
+				<th align="left">Address 1:</th>
+				<td align="left">
+					<input name="offer_it_map_address1"    type="text" size="32" value="<?php echo get_option('offer_it_map_address1'); ?>" />
+					(ex. bill_address). Client address
+				</td>
+			</tr>
+			<tr>
+				<th align="left">Address 2:</th>
+				<td align="left">
+					<input name="offer_it_map_address2"    type="text" size="32" value="<?php echo get_option('offer_it_map_address2'); ?>" />
+					(ex. bill_address2). Client address
+				</td>
+			</tr>
+			<tr>
+				<th align="left">City:</th>
+				<td align="left">
+					<input name="offer_it_map_city"        type="text" size="32" value="<?php echo get_option('offer_it_map_city'); ?>" />
+					(ex. bill_city). Client city
+				</td>
+			</tr>
+			<tr>
+				<th align="left">State:</th>
+				<td align="left">
+					<input name="offer_it_map_state"       type="text" size="32" value="<?php echo get_option('offer_it_map_state'); ?>" />
+					(ex. bill_state). Client state
+				</td>
+			</tr>
+			<tr>
+				<th align="left">Zip:</th>
+				<td align="left">
+					<input name="offer_it_map_zip"         type="text" size="32" value="<?php echo get_option('offer_it_map_zip'); ?>" />
+					(ex. bill_zip). Client zip code
+				</td>
+			</tr>
+			<tr>
+				<th align="left">Country:</th>
+				<td align="left">
+					<input name="offer_it_map_country"     type="text" size="32" value="<?php echo get_option('offer_it_map_country'); ?>" />
+					(ex. bill_country). Client country
+				</td>
+			</tr>
+			<tr>
+				<th align="left">Email:</th>
+				<td align="left">
+					<input name="offer_it_map_email"       type="text" size="32" value="<?php echo get_option('offer_it_map_email'); ?>" />
+					(ex. email). Client email
+				</td>
+			</tr>
+			<tr>
+				<th align="left">Phone:</th>
+				<td align="left">
+					<input name="offer_it_map_phone"       type="text" size="32" value="<?php echo get_option('offer_it_map_phone'); ?>" />
+					(ex. phone). Client phone
+				</td>
+			</tr>
+			<tr>
+				<th align="left">Custom 1:</th>
+				<td align="left">
+					<input name="offer_it_map_custom1"  type="text" size="32" value="<?php echo get_option('offer_it_map_custom1'); ?>" />
+					(ex. coupon). Custom information field
+				</td>
+			</tr>
+			<tr>
+				<th align="left">Custom 2:</th>
+				<td align="left">
+					<input name="offer_it_map_custom2"  type="text" size="32" value="<?php echo get_option('offer_it_map_custom2'); ?>" />
+					(ex. trans_id). Custom information field
+				</td>
+			</tr>
+			<tr>
+				<th align="left">Custom 3:</th>
+				<td align="left">
+					<input name="offer_it_map_custom3"  type="text" size="32" value="<?php echo get_option('offer_it_map_custom3'); ?>" />
+					(ex. notes). Custom information field
+				</td>
+			</tr>
+			<tr>
+				<th align="left">Custom 4:</th>
+				<td align="left">
+					<input name="offer_it_map_custom4"  type="text" size="32" value="<?php echo get_option('offer_it_map_custom4'); ?>" />
+					(ex. custom_field). Custom information field
+				</td>
+			</tr>
+			<tr>
+				<th align="left">Custom 5:</th>
+				<td align="left">
+					<input name="offer_it_map_custom5"  type="text" size="32" value="<?php echo get_option('offer_it_map_custom5'); ?>" />
+					(ex. additional_fields). Custom information field
+				</td>
+			</tr>
+
+
+			<tr>
+				<td colspan="2" align="left">
+					<h3>Logging / Debug</h3>
+				</td>
+			<tr>
+				<th align="left">Log activity:</th>
+				<td align="left">
+					<input name="offer_it_log" type="checkbox" value="1" <?php if(get_option('offer_it_log') == 1) echo 'checked="checked"'; ?> />
+					Turn on plugin log activity &nbsp;
+					<input type="submit" value="Show activity log" />
+					<input type="submit" value="Empty activity log" />
+				</td>
+			</tr>
 			</tr>
 		</table>
 		
-		<p><input type="submit" value="<?php _e('Save Changes') ?>" /></p>
+		<p><input type="submit" value="<?php _e('Save Changes') ?>" style="font-size: 4em" /></p>
 	</form>
 </div>
--- offer-it.class.php
+++ offer-it.class.php
@@ -15,13 +15,6 @@
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-require('php-ga-1.1.1/src/autoload.php');
-
-use UnitedPrototype\GoogleAnalytics;
-
-
-
-
 class offer_it {
 
 	public $wpdb    = null;		// WordPress DB hookup
@@ -41,27 +34,36 @@ class offer_it {
 
 		if(is_admin()):
 			add_action('admin_menu', array(&$this, 'offer_it_admin_menu'));
-			
-			
 		endif;
 
 
 		// install WP options
+		$check         = true;
 		$this->options = array(
 			'domain', 			// offerIT tracking domain
 			'username', 		// offerIT username
 			'apikey', 			// offerIT API key
-			'ga_code', 			// Google Analytics code
-			'ga_domain', 		// Google Analytics domain
-			'conversion_url', 	// conversion URL to trigger post
+			'tracking_code', 	// default ocode if non is detected on landing_url request
+			'landing_url', 		// landing URL to trigger redirection bounce vs OfferIT system
+			'conversion_url', 	// conversion URL to trigger post to OfferIT system
 			'conversion_ouid', 	// conversion unique variable
-			'mapper_get', 		// $_GET filed mapper to offerIT variables
-			'mapper_post', 		// $_POST field mapper to offerIT variables
+			'log'               // log plugin activity
 		);
 
-		foreach($this->options as $option)
-			$this->config[ $option ] = get_option('offer_it_' . $option);
+		$required = array(
+			'domain',
+			'username',
+			'apikey',
+			'tracking_code',
+			'landing_url',
+			'conversion_url',
+			'conversion_ouid'
+		);
 
+		foreach($this->options as $option) {
+			$this->config[ $option ] = trim(get_option('offer_it_' . $option));
+			if(in_array($option, $required) && !$this->config[ $option ]) $check = false;
+		}
 
 		// start PHP session if not present
 		if(!session_id()) session_start();
@@ -70,63 +72,43 @@ class offer_it {
 	    // store GET variables into a SESSION to be able to access later
 	    if(isset($_GET['ocode'])   && $_GET['ocode'])   $_SESSION['offer_it']['ocode']   = $_GET['ocode'];
 	    if(isset($_GET['transid']) && $_GET['transid']) $_SESSION['offer_it']['transid'] = $_GET['transid'];
-	
 
-	    // trigger Google Analytics tracking
-	    # $this->offer_it_track_pageview();
-	
+		// terminate plugin if required setup variables is missing
+		if($check === false) return $this;
 
-	    // query offerIT for affiliate details
-	    // only if ocode is present in URL string and not already stored in the session
-	    if(isset($_GET[ ['ocode'] ]) && $_GET[ ['ocode'] ] && $_GET[ ['ocode'] ] != $_SESSION['offer_it']['ocode']):
-			$url      = sprintf('http://%s/admin_api.php?wsdl', $this->config['domain']);
-			$client   = new nusoap_client($url, true);
-			$data     = array(
-				'ts' 		=> date('Y-m-d H:i:s'),
-				'request' 	=> $url
+#unset($_SESSION['offer_it']['transid']); // DEBUG: remove session transaction ID
+
+	    // trigger landing page track redirect if landing URL string is matched
+	    if(strpos($_SERVER['REQUEST_URI'], $this->config['landing_url']) !== false && !$_SESSION['offer_it']['transid']):
+
+			$ocode = $_SESSION['offer_it']['ocode'] ? $_SESSION['offer_it']['ocode'] : $this->config['tracking_code'];
+			$url   = 'http://' . str_replace('//', '/', sprintf('%s/track/%s/%s', $this->config['domain'], $ocode, $_SERVER['REQUEST_URI']));
+			$data  = array(
+				'address'  => $_SERVER['REMOTE_ADDR'],
+				'request'  => 'Landing URL request detected: ' . $_SERVER['REQUEST_URI'],
+				'response' => 'Redirecting to: ' . $url
 			);
 
-			
-			$client->setCredentials($this->config['username'], $this->config['apikey']);
+			if($this->config['log']) $this->wpdb->insert($this->wpdb->prefix . 'offerit_log', $data);
 
-			// Check for an API error
-			if($error = $client->getError()):
-				// store CURL post results in database
-				$data['response'] = $error;
-		
-				$this->wpdb->insert($this->wpdb->prefix . 'offerit_log', $data);
-
-				die('Offer IT plugin API call constructor error' . $error . "\n");
-			endif;
-
-
-		    $values = array('offeritcode' => $_SESSION['offer_it']['code']);
-			$_SESSION['offer_it']['decode']    = $client->call('decode_offeritcode', $values, 'offeritapiadmin_wsdl');
-			
-			$values = array('loginid' => $_SESSION['offer_it']['decode']['loginid']);
-			$_SESSION['offer_it']['affiliate'] = $client->call('list_aff_details',   $values, 'offeritapiadmin_wsdl');
-
-			$data['response'] = print_r($_SESSION['offer_it'], true);
-
-			$this->wpdb->insert($this->wpdb->prefix . 'offerit_log', $data);
+			header( 'Location: ' . $url ); die();
 	    endif;
 
-
-
-# $this->debug($this->config); $this->debug($_SESSION, true); // DEBUG
-
-	    // redirect URL to hide OfferIT variables is required
-	    # some code here
-
-
-	    // trigger conversion post if conversion URL string is detected
-	    if(strpos($_SERVER['REQUEST_URI'], $this->config['conversion_url']) !== false && isset($_GET[ $this->config['conversion_ouid'] ])):
+	    // trigger conversion post if conversion URL string is matched
+	    if(isset($_GET[ $this->config['conversion_ouid'] ]) && strpos($_SERVER['REQUEST_URI'], $this->config['conversion_url']) !== false):
 			// query database for order details by order unique ID
 			$query = sprintf('SELECT * FROM %s%s WHERE %s="%s" LIMIT 1', 
-					 $this->wpdb->prefix, $this->config['mapper_post']['table'], $this->config['mapper_post']['order'], $_GET[ $this->config['conversion_ouid'] ]);
-			$data  = $this->wpdb->get_row($query); # $this->debug($data, true); // DEBUG
+					 $this->wpdb->prefix, $this->config['db_table'], $this->config['db_column'], $_GET[ $this->config['conversion_ouid'] ]);
+			$order = $this->wpdb->get_row($query);
+			$data  = array(
+				'address'  => $_SERVER['REMOTE_ADDR'],
+				'request'  => 'Querying order from DB: ' . $query,
+				'response' => substr(print_r($order, true), 6)
+			);
 
-			if(is_object($data)) $this->offer_it_conversion($data);
+			if($this->config['log']) $this->wpdb->insert($this->wpdb->prefix . 'offerit_log', $data);
+
+			if(is_object($data)) $this->offer_it_conversion($order);
 	    endif;
 	    
 	    
@@ -142,9 +124,6 @@ class offer_it {
 	    
 		foreach($this->options as $option)
 			add_option( 'offer_it_' . $option, null );
-
-		// default hardcoded Cart66 mappings
-		update_option( 'offer_it_mapper_post', 'a:3:{s:5:"table";s:13:"cart66_orders";s:5:"order";s:4:"ouid";s:6:"fields";a:16:{s:9:"firstname";s:15:"bill_first_name";s:8:"lastname";s:14:"bill_last_name";s:8:"address1";s:12:"bill_address";s:8:"address2";s:13:"bill_address2";s:4:"city";s:9:"bill_city";s:5:"state";s:10:"bill_state";s:3:"zip";s:12:"bill_country";s:7:"country";s:8:"bill_zip";s:5:"email";s:5:"email";s:5:"phone";s:5:"phone";s:5:"gross";s:5:"total";s:7:"custom1";s:6:"coupon";s:7:"custom2";s:15:"discount_amount";s:7:"custom3";s:8:"trans_id";s:7:"custom4";s:12:"custom_field";s:7:"custom5";s:17:"additional_fields";}}');
 	}
 
 
@@ -161,31 +140,33 @@ class offer_it {
 
 	// post conversion pixel
 	function offer_it_conversion($data) {
+		if(!$_SESSION['offer_it']['transid'] || !$this->config['domain'] || !$this->config['conversion_ouid']) return false;
+	
 		$mapper    = $this->config['mapper_post']['fields'];
 		$post_url  = sprintf('http://%s/signup/process_pixel.php', $this->config['domain']);
 		$post_data = array(
 			'transid'   => $_SESSION['offer_it']['transid'],			// offerIT transaction ID
 			'orderid'   => $_GET[ $this->config['conversion_ouid'] ],	// order ID - has to be unique for OfferIT to accept conversion POST call
 
-			'gross'     => $data->$mapper['gross']    ,					// transaction total
+			'gross'     => $data->$this->config['map_total'],			// transaction total
 	
-			'firstname' => $data->$mapper['firstname'],
-			'lastname'  => $data->$mapper['lastname'] ,
-			'address1'  => $data->$mapper['address1'] ,
-			'address2'  => $data->$mapper['address2'] ,
-			'city'      => $data->$mapper['city']     ,
-			'state'     => $data->$mapper['state']    ,
-			'zip'       => $data->$mapper['zip']      ,
-			'country'   => $data->$mapper['country']  ,
+			'firstname' => $data->$this->config['map_first_name'],
+			'lastname'  => $data->$this->config['map_last_name'] ,
+			'address1'  => $this->config['map_address1'],
+			'address2'  => $this->config['map_address2'],
+			'city'      => $this->config['map_city']    ,
+			'state'     => $this->config['map_state']   ,
+			'zip'       => $this->config['map_zip']     ,
+			'country'   => $this->config['map_country'] ,
 	
-			'email'     => $data->$mapper['email']    ,
-			'phone'     => $data->$mapper['phone']    ,
+			'email'     => $this->config['map_email']   ,
+			'phone'     => $this->config['map_phone']   ,
 	
-			'custom1'   => $data->$mapper['custom1']  ,
-			'custom2'   => $data->$mapper['custom2']  ,
-			'custom3'   => $data->$mapper['custom3']  ,
-			'custom4'   => $data->$mapper['custom4']  ,
-			'custom5'   => $data->$mapper['custom5']  ,
+			'custom1'   => $this->config['map_custom1'] ,
+			'custom2'   => $this->config['map_custom2'] ,
+			'custom3'   => $this->config['map_custom3'] ,
+			'custom4'   => $this->config['map_custom4'] ,
+			'custom5'   => $this->config['map_custom5'] ,
 			'ip'     	=> $_SERVER['REMOTE_ADDR']
 		);
 
@@ -199,47 +180,21 @@ class offer_it {
 		curl_setopt($ch, CURLOPT_URL,             $post_url);
 		curl_setopt($ch, CURLOPT_POSTFIELDS,      http_build_query($post_data));
 		
-		$response = curl_exec($ch); # $this->debug($response, true); 
+		$response = curl_exec($ch);
 		
 		curl_close($ch);
 	
 
 		// store CURL post results in database
 		$data = array(
-			'ts' 		=> date('Y-m-d H:i:s'),
-			'request' 	=> substr(print_r($post_data, true), 6),
-			'response' 	=> $response
+			'address'  => $_SERVER['REMOTE_ADDR'],
+			'request'  => substr(print_r($post_data, true), 6),
+			'response' => $response
 		);
 
-		$this->wpdb->insert($this->wpdb->prefix . 'offerit_log', $data);
+		if($this->config['log']) $this->wpdb->insert($this->wpdb->prefix . 'offerit_log', $data);
 	}
 	
-	
-
-	// track with Google Analytics
-	function offer_it_track_pageview() {
-		if(!$this->config['ga_code'] || !$this->config['ga_domain']) return false;
-	
-		// Initilize GA Tracker
-#		$tracker = new GoogleAnalytics\Tracker($this->config['ga_code'], $this->config['ga_domain']);
-		
-		// Assemble Visitor information
-		$visitor = new GoogleAnalytics\Visitor();
-		$visitor->setIpAddress($_SERVER['REMOTE_ADDR']);
-		$visitor->setUserAgent($_SERVER['HTTP_USER_AGENT']);
-		$visitor->setScreenResolution('1024x768');
-		
-		// Assemble Session information
-#		$session = new GoogleAnalytics\Session();
-		
-		// Assemble Page information
-		$page    = new GoogleAnalytics\Page($_SERVER['REQUEST_URI']);
-		$page->setTitle(basename($_SERVER['REQUEST_URI']));
-		
-		// Track page view
-#		$tracker->trackPageview($page, $session, $visitor);
-	}
-
 	
 
 	// create table for logging API queries
@@ -247,7 +202,7 @@ class offer_it {
 	    $table = $this->wpdb->prefix . 'offerit_log';
 	    $query = "CREATE TABLE IF NOT EXISTS " . $table . " (
 				`id` int(11) NOT NULL AUTO_INCREMENT,
-				`ts` datetime DEFAULT NULL,
+				`ts` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
 				`request`  text COLLATE utf8_unicode_ci,
 				`response` text COLLATE utf8_unicode_ci,
 				PRIMARY KEY (`id`)
@@ -271,17 +226,6 @@ class offer_it {
 	function offer_it_admin_template() {
 		require('offer-it-admin.php');
 	}
-
-
-
-	// throw debug data arrays on the screen
-	function debug($data, $stop = false) {
-
-		echo '<pre>' . print_r($data, true) . '</pre>';
-
-		if($stop) die('DEBUG: STOP');
-	}
-
 } // END: OfferIT class
 
 ?>
